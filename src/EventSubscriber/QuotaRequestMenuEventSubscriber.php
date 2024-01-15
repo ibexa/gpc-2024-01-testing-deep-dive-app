086ac27870ee273d6ae3c3906b28b7e8c3341a72
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2024-01-15 15:01:11 +0100

    Added logging for admin menu subscriber - fixed PHPStan

--- src/EventSubscriber/QuotaRequestMenuEventSubscriber.php
+++ src/EventSubscriber/QuotaRequestMenuEventSubscriber.php
@@ -4,10 +4,14 @@ namespace App\EventSubscriber;
 
 use Ibexa\AdminUi\Menu\Event\ConfigureMenuEvent;
 use Ibexa\AdminUi\Menu\MainMenuBuilder;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-final class QuotaRequestMenuEventSubscriber implements EventSubscriberInterface
+final class QuotaRequestMenuEventSubscriber implements EventSubscriberInterface, LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -20,6 +24,15 @@ final class QuotaRequestMenuEventSubscriber implements EventSubscriberInterface
         $menu = $event->getMenu();
 
         $contentMenu = $menu[MainMenuBuilder::ITEM_ADMIN];
+        if ($contentMenu === null) {
+            $this->logger->warning(sprintf(
+                'Main admin menu is missing %s item. Failed to add %s menu position.',
+                MainMenuBuilder::ITEM_ADMIN,
+                'Quota requests',
+            ));
+
+            return;
+        }
 
         $contentMenu->addChild(
             'Quota requests',
