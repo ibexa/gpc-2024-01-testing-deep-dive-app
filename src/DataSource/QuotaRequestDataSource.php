e47773d7170e9cd62486365e6cfe30e8431cefc0
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2024-01-16 17:00:02 +0100

    Added basic permission check in QuotaRequestDataSource

--- src/DataSource/QuotaRequestDataSource.php
+++ src/DataSource/QuotaRequestDataSource.php
@@ -3,11 +3,26 @@
 namespace App\DataSource;
 
 use App\Data\QuotaRequest;
+use Ibexa\Contracts\Core\Repository\PermissionResolver;
 
 final class QuotaRequestDataSource implements QuotaRequestDataSourceInterface
 {
+    public function __construct(
+        private readonly PermissionResolver $permissionResolver)
+    {
+    }
+
+    /**
+     * @return iterable<QuotaRequest>
+     */
     public function findAll(): iterable
     {
+        $userId = $this->permissionResolver->getCurrentUserReference()->getUserId();
+        // $this->configResolver->getParameter('anonymous_user_id')
+        if ($userId === 10) {
+            return [];
+        }
+
         return [
             new QuotaRequest('Mike', 'mike@example.com', 'I need some of your stuff'),
             new QuotaRequest('Zoe', 'zoe@example.com', 'I need some of your stuff too!'),
--- tests/DataSource/QuotaRequestDataSourceTest.php
+++ tests/DataSource/QuotaRequestDataSourceTest.php
@@ -3,6 +3,8 @@
 namespace App\Tests\DataSource;
 
 use App\DataSource\QuotaRequestDataSourceInterface;
+use Ibexa\Contracts\Core\Repository\PermissionResolver;
+use Ibexa\Core\Repository\Values\User\UserReference;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 /**
@@ -12,17 +14,33 @@ final class QuotaRequestDataSourceTest extends KernelTestCase
 {
     private QuotaRequestDataSourceInterface $dataSource;
 
+    private PermissionResolver $permissionResolver;
+
     protected function setUp(): void
     {
         self::bootKernel();
-        $dataSource = $this->getContainer()->get(QuotaRequestDataSourceInterface::class);
-        assert($dataSource instanceof QuotaRequestDataSourceInterface);
-        $this->dataSource = $dataSource;
+        $repository = $this->getContainer()->get(QuotaRequestDataSourceInterface::class);
+        assert($repository instanceof QuotaRequestDataSourceInterface);
+        $this->dataSource = $repository;
+
+        $permissionResolver = $this->getContainer()->get(PermissionResolver::class);
+        assert($permissionResolver instanceof PermissionResolver);
+        $this->permissionResolver = $permissionResolver;
     }
 
     public function testFindAll(): void
     {
+        $this->permissionResolver->setCurrentUserReference(new UserReference(14));
+
         $results = $this->dataSource->findAll();
         self::assertCount(2, $results);
+    }
+
+    public function testFindAsAnonymousGivesNoResults(): void
+    {
+        $this->permissionResolver->setCurrentUserReference(new UserReference(10));
+
+        $results = $this->dataSource->findAll();
+        self::assertCount(0, $results);
     }
 }
