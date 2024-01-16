31ab6f004a1bd5f075b17f9e4f4ac1157f9b2c6b
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2024-01-16 13:45:41 +0100

    Removed bootstrap always creating database

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,41 +6,7 @@
  */
 declare(strict_types=1);
 
-use App\Kernel;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Dotenv\Dotenv;
-
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-chdir(dirname(__DIR__));
 
 $env = new Dotenv();
 $env->loadEnv(dirname(__DIR__) . '/.env');
-
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) ($_SERVER['APP_DEBUG'] ?? false));
-$kernel->boot();
-
-$application = new Application($kernel);
-$application->setAutoExit(false);
-
-if (isset($_SERVER['DATABASE_URL']) && 'sqlite' !== substr($_SERVER['DATABASE_URL'], 0, 6)) {
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:database:drop',
-        '--if-exists' => '1',
-        '--force' => '1',
-        '--quiet' => true,
-    ]));
-}
-
-$application->run(new ArrayInput([
-    'command' => 'doctrine:database:create',
-    '--quiet' => true,
-]));
-
-$application->run(new ArrayInput([
-    'command' => 'ibexa:install',
-    '--quiet' => true,
-]));
-
-$kernel->shutdown();
