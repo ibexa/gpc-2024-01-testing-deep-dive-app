9c6676b42e6fb5908a75d4e8354513152894a026
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2024-01-26 06:56:38 +0100

    Added basic QuotaRequest functionality - fixed PHPStan

--- src/DataSource/QuotaRequestDataSourceInterface.php
+++ src/DataSource/QuotaRequestDataSourceInterface.php
@@ -2,7 +2,12 @@
 
 namespace App\DataSource;
 
+use App\Data\QuotaRequest;
+
 interface QuotaRequestDataSourceInterface
 {
+    /**
+     * @return iterable<QuotaRequest>
+     */
     public function findAll(): iterable;
 }
